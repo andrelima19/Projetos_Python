a9abe33c6d4688708a7b2a3cc53c475d1fb9efad
André Lima <49543914+andrelima19@users.noreply.github.com>
2021-04-15 18:14:07 -0300

    Ex 75 - quase completo

---
+++ Developer/Python_Definitivo/Exercícios/Tuplas/Ex 75 – Análise de dados em uma Tupla.py
@@ -0,0 +1,19 @@
+# Exercício Python 075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+#
+# A) Quantas vezes apareceu o valor 9.
+#
+# B) Em que posição foi digitado o primeiro valor 3.
+#
+# C) Quais foram os números pares.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+n3 = int(input('Digite um número: '))
+n4 = int(input('Digite um número: '))
+
+x = n1, n2,n3,n4
+
+print(x.count(9))
+print(x.index(3))
+
+
