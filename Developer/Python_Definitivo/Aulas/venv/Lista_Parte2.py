a016de5860045dadd4fe809a9c13be4609458378
andrelima19 <a.andreluislima@gmail.com>
2021-07-09 01:03:13 -0300

    Listas parte 2

---
+++ Developer/Python_Definitivo/Aulas/venv/Lista_Parte2.py
@@ -0,0 +1,11 @@
+'''
+LISTAS PARTE 2
+Listas compostas - listas dentro de listas
+'''
+
+pessoas =[['Pedro',25],['Maria', 19], ['João', 32]]
+print(pessoas[0][0])
+print(pessoas[1][1])
+print(pessoas[1])
+
+###############
