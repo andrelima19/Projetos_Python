f787c1e5ec27c4dccfbb47e4c0682298d60ca5c6
andrelima19 <a.andreluislima@gmail.com>
2021-07-13 00:41:32 -0300

    Ex 85

--- Developer/Python_Definitivo/Exercícios/Listas/Ex 85 – Listas com pares e ímpares.py
+++ Developer/Python_Definitivo/Exercícios/Listas/Ex 85 – Listas com pares e ímpares.py
@@ -3,21 +3,22 @@
 # e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final,
 # mostre os valores pares e ímpares em ordem crescente.
 
-grupo_numeros = []
-temp = []
-pares = []
-impares = []
+grupo_numeros = [[],[]]
+num = 0
 
-grupo_numeros.append(pares)
-grupo_numeros.append(impares)
+for c in range(1,8):
+   num = int(input(f'Digite o {c}o valor: '))
 
-for c in range(0,6):
-   temp.append(int(input('Digite um número: '))) # Lista "temporaria" recebe os valores
-   grupo_numeros.append(temp[:]) # Lista "temporaria" é copiada sem vinculo [:] para a lista principal
+   if num % 2 == 0:
+      grupo_numeros[0].append(num)
+   else:
+      grupo_numeros[1].append(num)
 
-
-
+# A - Sete valores númericos
 print(f'Todos os números: {grupo_numeros}')
-print(pares)
-#print(pares)
-#print(impares)
+# B - Grupos de pares e ímpares
+print(f'Números pares: {grupo_numeros[0]}\n'
+      f'Números ímpares: {grupo_numeros[1]}')
+# C - Pares e ímpares em ordem crescente
+print(sorted(grupo_numeros[0]))
+print(sorted(grupo_numeros[1]))
