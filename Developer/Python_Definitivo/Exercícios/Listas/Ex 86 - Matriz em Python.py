13df38651d690c7bb08bfd2950a13fdea4668909
andrelima19 <a.andreluislima@gmail.com>
2021-07-15 18:03:27 -0300

    Praticando Matriz

--- Developer/Python_Definitivo/Exercícios/Listas/Ex 86 - Matriz em Python.py
+++ Developer/Python_Definitivo/Exercícios/Listas/Ex 86 - Matriz em Python.py
@@ -2,8 +2,24 @@
 #  Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores
 #  lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 
-matriz = [[0],[],[]]
-temp = []
+matriz = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
-
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}],[{c}]: '))
+print(f'Matriz [l]: {matriz[l]}')
+print(f'Matriz [c]: {matriz[c]}')
+print(matriz)
 print(matriz[0])
+print(matriz[1])
+print(matriz[2])
+print(matriz[3])
+
+
+#print('-=' * 30)
+
+#for l in range(0,4):
+#    for c in range(0,4):
+#       print(f'[{matriz[l][c]:^5}]',end=' ')
+#   print()
+
---
+++ Developer/Python_Definitivo/Exercícios/Listas/Ex 86 - Matriz em Python_Lab.py
@@ -0,0 +1,9 @@
+matriz = [[],[],[]] # Criação da matriz
+
+for lin in range(0,3):
+    for col in range(0,3):
+        matriz.append(int(input('Digite um valor: ')))
+        #matriz[0].append(int(input('Digite um valor: ')))
+        #matriz[lin].append(int(input('Digite um valor: ')))
+
+print(matriz)
