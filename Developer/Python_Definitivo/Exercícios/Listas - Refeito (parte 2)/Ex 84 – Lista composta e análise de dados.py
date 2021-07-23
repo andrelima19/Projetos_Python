e69af8354a181bd558a561e662d54ecf09bbc73c
andrelima19 <a.andreluislima@gmail.com>
2021-07-23 17:53:56 -0300

    mais de exercicios refeitos

--- Developer/Python_Definitivo/Exercícios/Listas - Refeito (parte 2)/Ex 84 – Lista composta e análise de dados.py
+++ Developer/Python_Definitivo/Exercícios/Listas - Refeito (parte 2)/Ex 84 – Lista composta e análise de dados.py
@@ -5,6 +5,7 @@
 # C) Uma listagem com as pessoas mais leves.
 
 grupo_pessoas = [[],[],[],[]]
+maior = menor = 0
 
 while True:
     nome = str(input('Nome: '))
@@ -12,13 +13,19 @@ while True:
     grupo_pessoas[0].append(nome)
     grupo_pessoas[1].append(peso)
 
+    if len(grupo_pessoas) == 0:
+        grupo_pessoas[1] = maior = menor
+    else:
+        if grupo_pessoas[1] > maior:
+            maior = grupo_pessoas[1]
+        if grupo_pessoas[1] < menor:
+            menor = grupo_pessoas[1]
 
     resp = str(input('Deseja continuar? [S/N] ')).strip().upper()
     if 'S' not in resp:
         break
 
-
-
 print(f'Lista de pessoas: {grupo_pessoas}\n'
       f'Nome: {grupo_pessoas[0]}\n'
-      f'Peso: {grupo_pessoas[1]}')
+      f'Peso: {grupo_pessoas[1]}\n')
+print(grupo_pessoas)
--- Developer/Python_Definitivo/Exercícios/Listas - Refeito (parte 2)/Ex 85 - Lista com Pares e Impares.py
+++ Developer/Python_Definitivo/Exercícios/Listas - Refeito (parte 2)/Ex 85 - Lista com Pares e Impares.py
@@ -3,3 +3,22 @@
 # e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final,
 # mostre os valores pares e ímpares em ordem crescente.
 
+lista_princ = [[],[]]
+
+while True:
+    n = int(input('Digite um valor: '))
+
+    if n % 2 == 0:
+        lista_princ[0].append(n)
+    else:
+        lista_princ[1].append(n)
+
+    resp = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    if 'S' not in resp:
+        break
+
+print(lista_princ)
+
+
+
+
