72bb3690b3e111646fcbec240da711114204a53b
andrelima19 <a.andreluislima@gmail.com>
2021-07-28 00:43:00 -0300

    dict

--- Developer/Python_Definitivo/Exercícios/Dicionários/Ex 92 – Cadastro de Trabalhador em Python.py
+++ Developer/Python_Definitivo/Exercícios/Dicionários/Ex 92 – Cadastro de Trabalhador em Python.py
@@ -4,3 +4,22 @@
 # o dicionário receberá também o ano de contratação e o salário.
 # Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 
+#nome, ano de nascimento, número ctps, ano de contratação, salário
+# Tempo para se aposentar
+
+carteira = dict()
+
+carteira['nome'] = str(input('Informe seu nome: '))
+carteira['ano de nascimento'] = int(input('Informe o ano em que nasceu: '))
+carteira['ctps'] = int(input('Número da carteira de trabalho (0 sai): '))
+carteira['ano de contratação'] = int(input('Ano de contratação: '))
+carteira['salario'] = float(input('Informe seu salário: '))
+
+carteira['tempo para aposentadoria'] = ( 2021 - carteira['ano de nascimento']) - ( 2021 - carteira['ano de contratação'])
+
+print(f'Nome: {carteira["nome"]}\n'
+      f'Ano de Nascimento: {carteira["ano de nascimento"]}\n'
+      f'CTPS: {carteira["ctps"]}\n'
+      f'Ano de contratação: {carteira["ano de contratação"]}\n'
+      f'Salário: { carteira ["salario"]}\n'
+      f'Tempo para se aposentar: {carteira["tempo para aposentadoria"]} anos')
--- Developer/Python_Definitivo/Exercícios/Dicionários/Ex 93 – Cadastro de Jogador de Futebol.py
+++ Developer/Python_Definitivo/Exercícios/Dicionários/Ex 93 – Cadastro de Jogador de Futebol.py
@@ -3,3 +3,14 @@
 # Depois vai ler a quantidade de gols feitos em cada partida.
 # No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
+#nome, partidas, gols, gols feitos no campeonato, aproveitamento.
+
+analise = dict()
+
+analise['nome'] = str(input('Nome: '))
+analise['partida'] = int(input('Jogos: '))
+
+for n in analise['partida']:
+
+    analise['gols'] = int(input(f'Partida: {n} - Gols feitos: {analise["gols"]}: '))
+print(f'{analise["partidas"]} - {analise["gols"]}')
