01b49e483c7af7335fea74561c0bc98c5c5e2548
andrelima19 <a.andreluislima@gmail.com>
2021-08-06 00:46:15 -0300

    labs função

---
+++ Developer/Python_Definitivo/Exercícios/Funções/labs.py
@@ -0,0 +1,27 @@
+# FUNÇÕES SEM PARÂMETROS
+def linhas():
+    print('-=' * 30)
+
+
+
+linhas()
+linhas()
+linhas()
+
+def soma():
+    print(100)
+
+soma()
+
+# FUNÇÕES COM PARÂMETROS
+
+def mensagem(txt): # Função recebe um parâmetro
+    print('*=' * 30)
+    print(txt)
+    print('*=' * 30)
+
+print()
+
+mensagem('CURSO EM VÍDEO')
+mensagem('Teste def')
+mensagem('Teste def 2')
